9ea7836e544c3b86937dce6dbffea9d51f6dcf1e
inverse-d <user@noemail.org>
2024-02-18 22:33:41 +0100

    Initial commit

--- main.go
+++ main.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"fmt"
-	"bufio"
-	"os"
-)	
+	"log"
+)
+
+func celsius (temp_f int) int {
+	temp_c := (temp_f -32) * 5/9
+	return temp_c
+}
+
+func fahrenheit (temp_c int) int {
+	temp_f := (temp_c * 9/5) + 32 
+	return temp_f
+}
 
 func main() {
 	fmt.Println("Temp Calculator")
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter 'f' for Fahrenheit or 'c' for Celsius: ")
-	mode, _ := reader.ReadString('\n')
-	fmt.Println("Mode: ", mode)
-
+	fmt.Println("Enter 'f' for Fahrenheit or 'c' for Celsius: ")
+	var mode string
+	_, err := fmt.Scan(&mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+	switch mode {
+	case "f":
+		fmt.Println("Type in the temperature in Celsius: ")
+		var temp_c int
+		fmt.Scan(&temp_c)
+		temp_f := fahrenheit(temp_c)
+		fmt.Println("Temperature in Fahrenheit: ", temp_f)
+	case "c":
+		fmt.Println("Type in the temperature in Fahrenheit: ")
+		var temp_f int
+		fmt.Scan(&temp_f)
+		temp_c := celsius(temp_f)
+		fmt.Println("Temperatur in Celsius: ", temp_c)
+	default:
+		fmt.Println("Undefined input")
+	}
 }
